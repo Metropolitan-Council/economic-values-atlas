fcbc98b8260c4c4c52afc9a23c7e71f04d51457a
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-04-09 16:30:57 -0500

    Update mod_plot_tract.R

--- R/mod_plot_tract.R
+++ R/mod_plot_tract.R
@@ -34,7 +34,7 @@ mod_plot_tract_server <- function(input, output, session,
   make_tract_avgs <- reactive({
     p <- map_util$plot_data2 %>%
     ungroup() %>%
-    st_drop_geometry() %>%
+    # st_drop_geometry() %>%
     group_by(name) %>% 
     summarise(MEAN = mean(opportunity_zscore, na.rm = T))
     return(p)
@@ -43,21 +43,47 @@ mod_plot_tract_server <- function(input, output, session,
   make_selected_vals <-  reactive({
     p <- map_util$plot_data2 %>%
     ungroup() %>%
-    st_drop_geometry() %>%
+    # st_drop_geometry() %>%
     filter(tr10 %in% tract_selections$selected_tract)
     return(p)
   })
   
-  output$bargraph <- renderPlot({
+  make_niceplot <- reactive({
+    p <- if(is.numeric(tract_selections$selected_tract)) {
       ggplot() +
-      geom_point(data = eva_vars, aes(y = name, x = n), col = "black") +
-      # geom_point(data = make_tract_avgs, aes(y = name, x = opportunity_zscore), col = "black") +
-      # geom_point(data = make_selected_vals, aes(y = name, x = opportunity_zscore), col = "blue") +
-      theme_bw() #+
+        geom_point(data = make_tract_avgs, aes(y = name, x = opportunity_zscore), col = "black") 
+    } else {
+      ggplot() +
+        geom_point(data = eva_vars, aes(y = name, x = n), col = "green")
+    }
+    return(p)
+  })
+  
+  # vals2 <- reactiveValues()
+  # 
+  # observe({
+  #   vals2$avgs <- make_tract_avgs()
+  # })
+  # 
+  # observe({
+  #   vals2$selected <- make_selected_vals()
+  # })
+  # 
+  # 
+  # return(vals2)
+  
+  
+  output$bargraph <- renderPlot({
+    print("making graph")
+    ggplot() + geom_point(aes(x = MEAN, y = name), data = make_tract_avgs())
+    # vals2$niceplot
+      # ggplot() +
+      #   geom_point(data = make_tract_avgs, aes(y = name, x = opportunity_zscore), col = "black") +
+      # # geom_point(data = make_selected_vals, aes(y = name, x = opportunity_zscore), col = "blue") +
+      # theme_bw() #+
       # scale_fill_brewer(palette = "Paired") +
       # facet_grid(Category~.,  space = "free_y", scales = "free_y") +
       # theme(legend.position = "bottom")
-    
   })
  
 }
